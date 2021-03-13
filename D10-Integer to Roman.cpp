53fc802b839c75d47b166be53ffe225f0e7423a3
sraj-99 <52272807+sraj-99@users.noreply.github.com>
2021-03-13 12:07:49 +0530

    Update D10-Integer to Roman.cpp

--- D10-Integer to Roman.cpp
+++ D10-Integer to Roman.cpp
@@ -1,1 +1,27 @@
-
+class Solution {
+public:
+    vector<string> c{"I","IV","V","IX","X","XL","L","XC","C","CD","D","CM","M"};
+    vector<int> a{1,4,5,9,10,40,50,90,100,400,500,900,1000};
+    string intToRoman(int num) {
+        string no;
+        int excepans=0;
+        unordered_map<int,string> m;
+        for(int i=0;i<a.size();i++)
+        {
+            m.insert(make_pair(a[i],c[i]));
+        }
+        while(num)
+        {
+            for(int i=12;i>=0;i--)
+            {
+               if(num>=a[i])
+               {
+                   no+=m[a[i]];
+                   num-=a[i];
+                   break;
+               }
+            }
+        }
+        return no;
+    }
+};
